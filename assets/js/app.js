bb7d8e29b414e9663e9974be68865160eb48f11b
HomS <homs_lainar@protonmail.com>
2021-04-21 23:11:19 -0500

    Finished modal form mobile view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,17 @@
 const hamburger= document.querySelector('.toggle');
+const btnCloseModal =  document.getElementById('btnCloseModal');
+const modalForm= document.querySelector('.modal');
+const btnsAddtoCart= document.querySelectorAll('.btn-cart');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
-})
+});
+
+btnCloseModal.addEventListener('click',()=>{
+    modalForm.classList.toggle('hidden');
+});
+
+btnsAddtoCart.forEach(btn => btn.addEventListener('click',(e)=>{
+    console.log(e.target.dataset.id);
+    modalForm.classList.toggle('hidden');
+}));
