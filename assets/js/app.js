7cacb43d798ce7bdd28d697fe5da55b0f7e87b25
HomS <homs_lainar@protonmail.com>
2021-04-30 10:15:58 -0500

    Cart modal , need to add show and update function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,73 @@
 const hamburger= document.querySelector('.toggle');
 const btnCloseModal =  document.getElementById('btnCloseModal');
-const modalForm= document.querySelector('.modal');
+const modalBox= document.querySelector('.modal');
 const btnsAddtoCart= document.querySelectorAll('.btn-cart');
+const modalForm = document.getElementById('modalForm');
+const shoppingList = document.getElementById('shopping-list');
+
+let actualProduct;
+const tBQuantity = document.getElementById('quantity');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
 });
 
 btnCloseModal.addEventListener('click',()=>{
-    modalForm.classList.toggle('hidden');
+    actualProduct = null;
+    modalBox.classList.toggle('hidden');
 });
 
 btnsAddtoCart.forEach(btn => btn.addEventListener('click',(e)=>{
-    console.log(e.target.dataset.id);
-    modalForm.classList.toggle('hidden');
+    actualProduct = e.target.dataset.id;
+    modalBox.classList.toggle('hidden');
 }));
+
+modalForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    if(actualProduct){
+        saveItem(actualProduct,tBQuantity.value);
+    }
+});
+
+
+function saveItem(productID,quantity){
+    quantity = parseInt(quantity);
+    let quantityInCart =  isItemInCart(productID);
+    if(quantityInCart){
+        quantity += parseInt(quantityInCart);
+    }
+    console.log(quantity);
+    localStorage.setItem(productID,quantity);
+
+}
+
+function isItemInCart(productID){
+    return localStorage.getItem(productID);
+}
+
+
+function showCartItems(){
+    const fragment = document.createDocumentFragment();
+
+    for(i=0;i<5;i++){
+        let box= document.createElement('div');
+        box.classList.add('cart__item');
+        let name= document.createElement('span');
+        let quantity= document.createElement('b');
+        let total = document.createElement('strong');
+
+        name.innerHTML = 'Product Name';
+        quantity.innerHTML = '5';
+        total.innerHTML = '$5000';
+
+        box.appendChild(name);
+        box.appendChild(quantity);
+        box.appendChild(total);
+        fragment.appendChild(box);
+    }
+
+    shoppingList.appendChild(fragment);
+
+}
+
+showCartItems();
